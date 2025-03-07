1e6e832cd74ea26db74a2f93d5b7256b30469b50
guilhermeKramer <guilherme_ok@hotmail.com>
2025-03-06 22:54:20 -0300

    feat: frontend app already up

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,15 @@ const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
 
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
 export default app;
