b6fe4b44e7d83fb22faab1072081164ff39034eb
guilhermeKramer <guilherme_ok@hotmail.com>
2025-03-06 22:21:30 -0300

    initial commit: setup project, backend and postgres services running

---
+++ backend/src/models/user/user.js
@@ -0,0 +1,20 @@
+
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.ENUM("user", "admin"), defaultValue: "user" },
+  },
+  {
+    timestamps: true, // Adiciona createdAt e updatedAt automaticamente
+    tableName: "users",
+  }
+);
+
+module.exports = User;
---
+++ backend/src/routes/userRouter.js
@@ -0,0 +1,12 @@
+import * as express from "express";
+import * as userController from "../controllers/userController.js";
+
+const router = express.Router();
+// router.get("/", userController.getAllUsers);
+// router.post("/register", userController.createUser);
+// router.post("/login", userController.loginUser);
+// router.get("/:id", userController.getUserById);
+// router.put("/:id", userController.updateUser);
+// router.delete("/:id", userController.deleteUser);
+
+export default router;
